d82800f36cb935d9532a401effa6c0e4f1c5b2b3
Oksana TrYGUB <ok.trygub@gmail.com>
2021-10-13 23:20:18 +0300

    updated

--- home-work-1.2/js/index.js
+++ home-work-1.2/js/index.js
@@ -1,35 +1,48 @@
-//There are diffent ways to accomplish this task
+//There are several ways to perform string to number for the correct addition operation.
+
 //Using +
 
-let num1 = +prompt("Give me the first operand");
-let num2 = +prompt("Give me the second operand");
+let num1ForAddition = +prompt("Give me the first operand");
+let num2ForAddition = +prompt("Give me the second operand");
 
-let result = num1 + num2;
+const resultOfAddition = num1ForAddition + num2ForAddition;
 
-console.log(result, typeof result)
+console.log(resultOfAddition);
 
-
-//Using Number()
+//Using Number() 
 
 /*
-let num1 = Number(prompt("Give me the first operand"));
-let num2 = Number(prompt("Give me the second operand"));
+let num1ForAddition = Number(prompt("Give me the first operand"));
+let num2ForAddition = Number(prompt("Give me the second operand"));
 
-let result = num1 + num2;
+const resultOfAddition = num1ForAddition + num2ForAddition;
 
-console.log(result, typeof result)
+console.log(resultOfAddition);
 */
 
 
-//Using parseInt
 
-/*
-let num1 = prompt("Give me the first operand");
-num1 = parseInt(num1, 10);
-let num2 = prompt("Give me the second operand");
-num2 = parseInt(num2, 10);
+let num1ForSubtraction = prompt("Give me the first operand");
+let num2ForSubtraction = prompt("Give me the second operand");
 
-let result = num1 + num2;
+const resultOfSubtraction = num1ForSubtraction - num2ForSubtraction;
 
-console.log(result, typeof result)
-*/
+console.log(+resultOfSubtraction.toFixed(2));
+
+
+
+let num1ForMultiplication = prompt("Give me the first operand");
+let num2ForMultiplication = prompt("Give me the second operand");
+
+const resultOfMultiplication = num1ForMultiplication * num2ForMultiplication;
+
+console.log(+resultOfMultiplication.toFixed(2));
+
+
+
+let num1ForDivision = prompt("Give me the first operand");
+let num2ForDivision = prompt("Give me the second operand");
+
+const resultOfDivision = num1ForDivision / num2ForDivision;
+
+console.log(+resultOfDivision.toFixed(2));
