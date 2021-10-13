9b7c3da5dae8a9ff61d5f6b15f0c948c2112a9cc
Oksana TrYGUB <ok.trygub@gmail.com>
2021-10-13 23:18:04 +0300

    updated

---
+++ home-work-1.3/js/index.js
@@ -0,0 +1,27 @@
+// Task #1
+
+let admin;
+let name = 'Джон';
+
+admin = name;
+
+alert(admin);
+
+
+//Task #2 
+
+/*
+let ourPlanetName = "Земля";
+let currentUserName = "Кcения";
+*/
+
+
+//Task #3
+
+let sum = '10' + 10;
+console.log(sum, typeof sum);
+
+/*
+If at least one operand is a string, then the second will also be converted to a string. It doesn't matter if the first or second operand is a string. String concatenation and transformation is a feature of the binary plus +. Other arithmetic operators only work with numbers and always convert their operands to numbers.
+
+*/
