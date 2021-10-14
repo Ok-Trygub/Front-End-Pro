af853d5a87899fcb4c2bf5e7da6b71e92d1346a1
Oksana TrYGUB <ok.trygub@gmail.com>
2021-10-14 13:38:34 +0300

    updated

--- home-work-1.2/js/index.js
+++ home-work-1.2/js/index.js
@@ -2,8 +2,8 @@
 
 //Using +
 
-let num1ForAddition = +prompt("Give me the first operand");
-let num2ForAddition = +prompt("Give me the second operand");
+const num1ForAddition = +prompt("Give me the first operand");
+const num2ForAddition = +prompt("Give me the second operand");
 
 const resultOfAddition = num1ForAddition + num2ForAddition;
 
@@ -15,15 +15,16 @@ console.log(resultOfAddition);
 let num1ForAddition = Number(prompt("Give me the first operand"));
 let num2ForAddition = Number(prompt("Give me the second operand"));
 
-const resultOfAddition = num1ForAddition + num2ForAddition;
+const resultOfAddition = num1ForAddition + num2ForAddition;34
+
 
 console.log(resultOfAddition);
 */
 
 
 
-let num1ForSubtraction = prompt("Give me the first operand");
-let num2ForSubtraction = prompt("Give me the second operand");
+const num1ForSubtraction = prompt("Give me the first operand");
+const num2ForSubtraction = prompt("Give me the second operand");
 
 const resultOfSubtraction = num1ForSubtraction - num2ForSubtraction;
 
@@ -31,8 +32,8 @@ console.log(+resultOfSubtraction.toFixed(2));
 
 
 
-let num1ForMultiplication = prompt("Give me the first operand");
-let num2ForMultiplication = prompt("Give me the second operand");
+const num1ForMultiplication = prompt("Give me the first operand");
+const num2ForMultiplication = prompt("Give me the second operand");
 
 const resultOfMultiplication = num1ForMultiplication * num2ForMultiplication;
 
@@ -40,8 +41,8 @@ console.log(+resultOfMultiplication.toFixed(2));
 
 
 
-let num1ForDivision = prompt("Give me the first operand");
-let num2ForDivision = prompt("Give me the second operand");
+const num1ForDivision = prompt("Give me the first operand");
+const num2ForDivision = prompt("Give me the second operand");
 
 const resultOfDivision = num1ForDivision / num2ForDivision;
 
