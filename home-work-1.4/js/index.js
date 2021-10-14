0aaa289647e985e2b9a4d42bcb44c3b6d27686a8
Oksana TrYGUB <ok.trygub@gmail.com>
2021-10-14 13:45:16 +0300

    updated

--- home-work-1.4/js/index.js
+++ home-work-1.4/js/index.js
@@ -1,18 +1,18 @@
-let operation = prompt('What operation do you want to do? (+, -, *, /)');
-let num1 = prompt('Enter the first number:');
-let num2 = prompt('Enter the second number:');
+const operation = prompt('What operation do you want to do? (+, -, *, /)');
+const num1 = prompt('Enter the first number:');
+const num2 = prompt('Enter the second number:');
 
 if (operation == '+') {
-	let result = (Number(num1) + Number(num2));
+	const result = (Number(num1) + Number(num2));
 	alert(`${num1} ${operation} ${num2} = ${result}`);
 } else if (operation == '-') {
-	let result = (num1) - (num2);
+	const result = (num1) - (num2);
 	alert(`${num1} ${operation} ${num2} = ${result}`);
 } else if (operation == '*') {
-	let result = (num1) * (num2);
+	const result = (num1) * (num2);
 	alert(`${num1} ${operation} ${num2} = ${result}`);
 } else if (operation == '/') {
-	let result = (num1) / (num2);
+	const result = (num1) / (num2);
 	alert(`${num1} ${operation} ${num2} = ${result}`);
 } else {
 	alert('Error. Please try again.');
