a6573d36fe9353600cb02d846e9cd94ad37851b1
Oksana TrYGUB <ok.trygub@gmail.com>
2021-10-21 16:57:18 +0300

    add

---
+++ home-work-7/js/index.js
@@ -0,0 +1,34 @@
+let numOrStr = prompt('input number or string');
+
+if (numOrStr === null || numOrStr.trim() === '' || isNaN(numOrStr)) {
+	alert('Please try again and input some number');
+} else {
+	alert('Very good!');
+}
+
+switch (numOrStr) {
+	case null:
+		alert('Error. Null');
+		break;
+	default:
+		alert('Very good!');
+		break;
+}
+
+switch (numOrStr.trim()) {
+	case '':
+		alert('Error. Empty String');
+		break;
+	default:
+		alert('Very good!');
+		break;
+}
+
+switch (isNaN(numOrStr)) {
+	case true:
+		alert('Error. Input only number.');
+		break;
+	default:
+		alert('Very good!');
+		break;
+}
