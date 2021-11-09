360bc388a8e7e3675a09b6993cc62f73dffdbb23
Ok-TRygub <ok.trygub@gmail.com>
2021-11-10 00:00:42 +0200

    updated

--- home-work-14/js/index.js
+++ home-work-14/js/index.js
@@ -1,12 +1,14 @@
 const userNum = +prompt("Введите число:", "6");
 const userDeg = +prompt("Степень, в которую это число нужно возвести:", "2");
 
-function degOfNum(num, deg = 1) {
-  if (!num || !deg) {
-    return "some error";
-  } else {
-    return Math.pow(num, deg);
-  }
+function degOfNum (num, deg = 1) {
+	if (num === 0 || deg === 0) {
+		return "Number must be more than 0";
+	} else if (!num || !deg) {
+		return "some error";
+	} else {
+		return Math.pow(num, deg);
+	}
 }
 const result = degOfNum(userNum, userDeg);
 alert(result);
