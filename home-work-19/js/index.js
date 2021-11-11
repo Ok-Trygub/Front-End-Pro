bc3449aa5dda1434e902c7849892e7716b212fdf
Ok-TRygub <ok.trygub@gmail.com>
2021-11-11 18:27:39 +0200

    add

---
+++ home-work-19/js/index.js
@@ -0,0 +1,108 @@
+// #1
+// const arr1 = [1, 2, 3];
+// const arr2 = [4, 5, 6];
+
+// const arr3 = arr1.concat(arr2);
+
+// alert(arr3);
+
+// #2
+
+// const arr = [1, 2, 3];
+// arr.reverse();
+
+// alert(arr);
+
+// #3
+
+// const arr = [1, 2, 3];
+// arr.push(4, 5, 6);
+
+// alert(arr);
+
+// #4
+
+// const arr = [1, 2, 3];
+// arr.unshift(4, 5, 6);
+
+// alert(arr);
+
+// #5
+
+// const arr = ['js', 'css', 'jq'];
+// alert(arr[0]);
+
+// #6
+
+// const arr = ['js', 'css', 'jq'];
+// alert(arr[arr.length-1]);
+
+// #7
+
+// const arr =[1, 2, 3, 4, 5];
+// const newArr = arr.slice(0, 3);
+
+// alert(newArr);
+
+// #8
+
+// const arr =[1, 2, 3, 4, 5];
+// const newArr = arr.slice(-2);
+
+// alert(newArr);
+
+// #9
+
+// const arr =[1, 2, 3, 4, 5];
+// arr.splice(1, 2);
+
+// alert(arr);
+
+// #10
+
+// const arr =[1, 2, 3, 4, 5];
+// const newArr = arr.splice(1, 3);  //if necessary cut values from arr
+// const newArr = arr.slice(1, 4);  //if necessary copy values from arr
+
+// alert(newArr);
+
+// #11
+
+// const arr = [1, 2, 3, 4, 5];
+// arr.splice(3, 0, "a", "b", "c");
+
+// alert(arr);
+
+// #12
+
+// const arr = [1, 2, 3, 4, 5];
+// arr.splice(1, 0, "a", "b");
+// arr.splice(6, 0, "c");
+// arr.splice(8, 0, "e");
+
+// alert(arr);
+
+//or
+
+// const arr = [1, 2, 3, 4, 5];
+// arr.splice(1, 0, "a", "b");
+// arr.splice(6, 0, "c");
+// arr.splice([arr.length], 0, "e");
+
+// alert(arr);
+
+// #13
+
+// const arr = [3, 4, 1, 2, 7];
+// arr.sort();
+
+// alert(arr);
+
+// #14
+
+// const arr = [5, 6, 7, 8, 9];
+// arr.sort();
+
+// alert(arr);
+
+// #15
