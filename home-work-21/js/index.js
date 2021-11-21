1ac058fa2f2337f22f88bbc2d40774e3822f28c2
Ok-TRygub <ok.trygub@gmail.com>
2021-11-21 21:36:54 +0200

    add

--- home-work-21/js/index.js
+++ home-work-21/js/index.js
@@ -1,4 +1,4 @@
-// #1 indexOf
+// #1 indexOf()
 
 // const arr = [12, 77, null, 11, "hi", 12, 77, null, 11, "hi", NaN];
 
@@ -17,7 +17,7 @@
 
 // console.log(indexOf(11));
 
-// #2 lastIndexOf
+// #2 lastIndexOf()
 
 // const arr = [12, 77, null, 11, "hi", 12, 77, null, 11, "hi", NaN];
 
@@ -32,21 +32,86 @@
 
 // console.log(lastIndexOf(11, -4));
 
-// #3 includes
+// #3 includes()
 
 // const arr = [12, 77, null, 11, "hi", NaN];
 
 // function includes (item, from = 0) {
-// 	for (let i = from; i < arr.length; i++) {
-
-// 		if (arr[i] === item) return true;
-
-
-// 		if (Number.isNaN(arr[i])) return true;
+// 	if (from >= 0) {
+// 		for (let i = from; i < arr.length; i++) {
+// 			if (arr[i] === item || Number.isNaN(arr[i])) return true;
+// 		}
+// 	} else if (from < 0) {
+// 		for (let i = arr.length + from; i < arr.length; i++) {
+// 			if (arr[i] === item) return true;
+// 		}
 // 	}
 // 	return - 1
 // }
-// console.log(includes("hui"));
+// console.log(includes(77, -5));
+
+// #4 find()
+
+const arr = [1, 2, -3, 4, 5, -6];
+
+function find (cb, array) {
+	let value = undefined;
+
+	for (let i = 0; i < array.length; i++) {
+		if (cb(array[i], i, array)) value = array[i];
+	}
+
+	return value;
+}
+
+function cb (value, index, array) {
+ if (value < 0) return true;
+}
+
+console.log(find(cb, arr));
+
+// #5 findIndex()
+
+// const arr = [1, 2, 3, -4, 5];
+
+// function findIndex (cb, array) {
+// 	let value = -1;
+
+// 	for (let i = 0; i < array.length; i++) {
+// 		if (cb(array[i], i, array)) value = i;
+// 	}
+
+// 	return value;
+// }
+
+// function cb (value, index, array) {
+//  if (value < 0) return true;
+// }
+
+// console.log(findIndex(cb, arr));
+
+// #6 every()
+
+// const arr = [1, 2, 3, -4, 5];
+
+// function every (cb, array) {
+// 	let value = false;
+
+// 	for (let i = 0; i < array.length; i++) {
+// 		if (cb(array[i], i, array)) value = true;
+// 	}
+// 	return value;
+// }
+
+// function cb (value, index, array) {
+//  if (value > 0) {
+// 	return true;
+//  } else {
+// 	 return false;
+//  }
+// }
+
+// console.log(every(cb, arr));
 
 
 
