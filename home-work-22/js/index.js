b79eb8a2df5687654f4bd5293e8dcaf1a3d7b9a2
Ok-Trygub <ok.trygub@gmail.com>
2021-12-01 21:15:35 +0200

    add

--- home-work-22/js/index.js
+++ home-work-22/js/index.js
@@ -1,30 +1,20 @@
-
-// Создайте функцию counter, которая будет создавать счетчик и иметь один необязательный параметр типа number.
-
+//  Создайте функцию counter, которая будет создавать счетчик и иметь один необязательный параметр типа number.
 // 	Вызов функции должен возвращать объект (счетчик) с тремя методами: increase, decrease, value.
-//
 // 	Вызов метода value должен возвращать текущее значение счетчика, increse – увеличивать его на 1, а decrease – уменьшать.
-//
 // 	Вызов функции counter должен создавать независимый счетчик – это значит, что вызов методов одного счетчика никак не
 // 	должен влиять на любой другой счетчик.
 
-
-
-function counter(value){
-	let accum = {
-		increase: (value) => value + 1,
-		decrease: (value) => value - 1,
-		value: (value) => value
-	}
-
-	return accum;
-	console.log(this.increase);
+function counter(num = 0) {
+  const accum = num;
+  return {
+    incr: () => accum + 1,
+    decr: () => accum - 1,
+    value: () => accum,
+  };
 }
+const res = counter(7);
+console.log(res.incr());
+console.log(res.decr());
+console.log(res.value());
 
 
-
-
-console.log(counter(2));
-
-
-phh
