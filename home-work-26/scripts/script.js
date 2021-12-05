d43d15c69d311b672bc717244772845cdc6400ba
Ok-Trygub <ok.trygub@gmail.com>
2021-12-05 21:09:29 +0200

    add

--- home-work-26/scripts/script.js
+++ home-work-26/scripts/script.js
@@ -1,25 +1,59 @@
-function random() {
-    const appearedNumbers = [];
+function sum(accum = 0) {
+	const statistic = {
+		increase: 0,
+		decrease: 0,
+		get: 0,
+		getStatistic: 0
+	};
 
-    function generateNum() {
-        if (appearedNumbers.length === 100) return appearedNumbers;
+	return {
+		increase(num) {
+			statistic.increase++;
+			return accum += num;
+		},
 
-        const num = Math.round(Math.random() * (100 - 1) + 1);
-        if (!appearedNumbers.includes(num)) {
-            appearedNumbers.push(num);
-        } else {
-            return generateNum();
-        }
-        return appearedNumbers;
-    }
-    return generateNum;
+		decrease(num) {
+			statistic.decrease++;
+			return accum -= num;
+		},
+
+		get() {
+			statistic.get++;
+			return accum;
+		},
+
+		getStatistic() {
+			statistic.getStatistic++;
+			return statistic;
+		},
+
+		clearAccum() {
+			return accum = 0;
+		},
+
+		clearStatistic() {
+			for (const key in statistic) {
+				statistic[key] = 0;
+			}
+			return statistic;
+		},
+
+		clearAll() {
+			this.clearAccum();
+			this.clearStatistic();
+			return {
+				accum,
+				statistic
+			}
+		}
+	};
 }
 
-const result = random();
+const result = sum();
+console.log(result.increase(4));
+console.log(result.decrease(1));
+console.log(result.decrease(1));
+console.log(result.get());
+console.log(result.getStatistic());
 
-
-for (let i = 0; i < 100; i++) {
-    console.log(result());
-}
-
-
+console.log(result.clearAll());
