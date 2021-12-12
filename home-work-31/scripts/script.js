d8550ed86cc16f0e70911826c8fc2c55fe4f6ec0
Ok-Trygub <ok.trygub@gmail.com>
2021-12-12 21:28:54 +0200

    updated

--- home-work-31/scripts/script.js
+++ home-work-31/scripts/script.js
@@ -2,10 +2,8 @@ const arr = [9, [5, 2], 4, [3, 1, [7, 8]]];
 const arr2 = [12, 4, 8, 7, 34];
 
 function flat(array) {
-	arguments.length === 1;
-
 	try {
-		if (arguments.length === 2) throw new Error('Function accepts only 1 argument, too much arguments provided');
+		if (arguments.length > 1) throw new Error('Function accepts only 1 argument, too much arguments provided');
 		let newArr = [];
 
 		for (let i = 0; i < array.length; i++) {
