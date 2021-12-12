ee21f373b5693cbfa169a03e36d9312eaec56d12
Ok-Trygub <ok.trygub@gmail.com>
2021-12-12 21:21:30 +0200

    add

---
+++ home-work-31/scripts/script.js
@@ -0,0 +1,76 @@
+const arr = [9, [5, 2], 4, [3, 1, [7, 8]]];
+const arr2 = [12, 4, 8, 7, 34];
+
+function flat(array) {
+	arguments.length === 1;
+
+	try {
+		if (arguments.length === 2) throw new Error('Function accepts only 1 argument, too much arguments provided');
+		let newArr = [];
+
+		for (let i = 0; i < array.length; i++) {
+
+			if (Array.isArray(array[i])) {
+				newArr = newArr.concat(flat(array[i]));
+			} else {
+				newArr.push(array[i]);
+			}
+		}
+		return newArr;
+	}
+	catch (err) {
+		console.error('Error: ' + err.message);
+	}
+}
+
+console.log(flat(arr));
+console.log(flat(arr2));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const arr = [9, [5, 2], 4, [3, 1, [7, 8]]];
+// const arr2 = [12, 4, 8, 7, 34];
+
+// function flat(array) {
+
+
+// 	let newArr = [];
+
+// 	for (let i = 0; i < array.length; i++) {
+
+// 		if (Array.isArray(array[i])) {
+// 			newArr = newArr.concat(flat(array[i]));
+// 		} else {
+// 			newArr.push(array[i]);
+// 		}
+// 	}
+// 	return newArr;
+// }
+
+// console.log(flat(arr));
+// console.log(flat(arr2));
+
+
+
+
+// 	return console.log('throw new Error(Function accepts only 1 argument, too much arguments provided)')
+
+
