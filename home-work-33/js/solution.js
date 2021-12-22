1adafc959a825527c1ac3a158c917f3f5f44e3e5
Ok-Trygub <ok.trygub@gmail.com>
2021-12-22 23:19:44 +0200

    add

---
+++ home-work-33/js/solution.js
@@ -0,0 +1,10 @@
+const elemList = document.body.querySelectorAll('li');
+console.log(elemList.length);
+
+const arr = [];
+
+for (let elem of elemList){
+	arr.push(elem.textContent);
+}
+console.log(arr);
+
