1536dad548a893bf4ac7c95a4ad0f3b269751a54
Ok-Trygub <ok.trygub@gmail.com>
2022-03-28 21:00:55 +0300

    add

--- home-work-24/scripts/script.js
+++ home-work-24/scripts/script.js
@@ -1,40 +1,39 @@
 function getRandomIndex(arrLength, usedPositions) {
-    const randomPosition = Math.floor(Math.random() * (arrLength + 1));
+	const randomPosition = Math.floor(Math.random() * (arrLength + 1));
 
-    if (usedPositions.includes(randomPosition)) {
-        return getRandomIndex(arrLength, usedPositions);
-    }
-    return randomPosition;
+	if (usedPositions.includes(randomPosition)) {
+			return getRandomIndex(arrLength, usedPositions);
+	}
+	return randomPosition;
 }
 
 function insertWordInto(str) {
-    const arr = str.split(' ');
-    let usedPositions = [];
+	const arr = str.split(' ');
+	let usedPositions = [];
 
-    return function (word) {
-        if (usedPositions.length === arr.length + 1) usedPositions = [];
-        const randomPosition = getRandomIndex(arr.length, usedPositions);
+	return function (word) {
+			if (usedPositions.length === arr.length + 1) usedPositions = [];
+			const randomPosition = getRandomIndex(arr.length, usedPositions);
 
-        if (arr.includes(word)) {
-            arr.splice(arr.indexOf(word), 1)
-        }
+			if (arr.includes(word)) {
+					arr.splice(arr.indexOf(word), 1)
+			}
 
-        arr.splice(randomPosition, 0, word);
+			arr.splice(randomPosition, 0, word);
 
-        if (!usedPositions.includes(randomPosition)) {
-            usedPositions.push(randomPosition);
-        }
+			if (!usedPositions.includes(randomPosition)) {
+					usedPositions.push(randomPosition);
+			}
 
-        let newString = arr.join(' ');
-        arr.splice(randomPosition, 1);
-        return newString;
-    }
+			let newString = arr.join(' ');
+			arr.splice(randomPosition, 1);
+			return newString;
+	}
 }
 
 const result = insertWordInto('0000 0000 0000 0000');
 
 for (let i = 0; i < 15; i++) {
-    if (i % 5 === 0) console.log('----');
-    console.log(result('|||||'));
+	if (i % 5 === 0) console.log('----');
+	console.log(result('|||||'));
 }
-
